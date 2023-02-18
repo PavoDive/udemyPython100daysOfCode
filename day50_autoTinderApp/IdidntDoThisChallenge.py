ab4be35f97e6f41e8fbe156744e4679d07532a55
pavodive <giovanni.pavolini@gmail.com>
2023-02-18 12:24:27 -0500

    Day 50 Auto Tinder App
    I didn't complete this challenge.

---
+++ day50_autoTinderApp/IdidntDoThisChallenge.py
@@ -0,0 +1,7 @@
+# I didn't do this challenge because of a number of reasons:
+
+# 1. I find it ethically questionable to either like or dislike someone who elected to expose themselves on a matching app.
+# 2. I find it ethically questionable to inflate tinder's numbers, and hence value, by completing this assignment.
+
+# While I understand --and believe-- that there are important lessons to be learned from this challenge, I'm willing
+# to accept a small delay and learn those skills and concepts by doing some other, less questionable assignments.
