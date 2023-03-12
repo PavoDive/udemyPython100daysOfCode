a3c1ff8cea0f78dd71173e59521a47245d65405d
pavodive <giovanni.pavolini@gmail.com>
2023-03-12 15:09:08 -0500

    Remove one-digit folders
    Remove folders with one-digit names.

--- day2_tipCalculator/tipCalculator.py
+++
@@ -1,8 +0,0 @@
-bill = float(input("What was the total bill? $"))
-tip = int(input("What would you like to tip: 10, 12 or 15? "))
-people = int(input("How many people are splitting the bill? "))
-
-total_bill = bill * (1 + tip / 100)
-individual_bill = total_bill / people
-
-print(f"Each person should pay: ${'{:.2f}'.format(individual_bill)}")
