4c62e05bc87b88ac8bb260d08e95c1cfab21b0d7
pavodive <giovanni.pavolini@gmail.com>
2023-03-12 22:17:35 -0500

    Day 60 Handling Post Requests with Flask
    Capturing form data.

---
+++ day60_POSTrequestsFlask/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods = ["POST"])
+def receive_data():
+    return f"<h1>Name: {request.form['username']}; Password: {request.form['password']}</h1>"
+    
+
+    
+if __name__ == "__main__":
+    app.run(debug = True)
+
